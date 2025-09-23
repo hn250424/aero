7f157e6f427fd09b42c0a8c346e894511f00a3a7
hn250424 <nami0879@naver.com>
2025-09-23 14:48:59 +0900

    spinbox

--- examples/numeric_input.ts
+++ examples/numeric_input.ts
@@ -1,8 +1,8 @@
 import * as aero from '../src'
-const dynamicContainer = document.getElementById('app-dynamic')
+// const dynamicContainer = document.getElementById('app-dynamic')
 
 
 
-const numericInput = new aero.NumericInput()
-dynamicContainer?.appendChild(numericInput)
+// const numericInput = new aero.NumericInput()
+// dynamicContainer?.appendChild(numericInput)
 
--- src/components/spinbox/Spinbox.ts
+++ src/components/spinbox/Spinbox.ts
@@ -1,69 +1,46 @@
 import spinboxTemplateHtml from './spinbox.html?raw'
+import BaseNumericInput from '../../base/BaseNumericInput'
 
-export default class Spinbox extends HTMLElement {
-    private input!: HTMLInputElement
-    private minus!: HTMLButtonElement
-    private plus!: HTMLButtonElement
-
-    private decimalPlaces!: number
-
-    // slider?: HTMLInputElement
+export default class Spinbox extends BaseNumericInput {
+    private minus: HTMLElement
+    private plus: HTMLElement
 
     constructor() {
         super()
-
-        const spinboxTemplate = document.createElement('template')
-        spinboxTemplate.innerHTML = spinboxTemplateHtml
+        
+        const template = document.createElement('template')
+        template.innerHTML = spinboxTemplateHtml
 
         const shadow = this.attachShadow({ mode: 'open' })
-        shadow.appendChild(spinboxTemplate.content.cloneNode(true))
+        shadow.appendChild(template.content.cloneNode(true))
 
         this.input = shadow.querySelector('.input')!
-        this.minus = shadow.querySelector('.minus')!
-        this.plus = shadow.querySelector('.plus')!
-
-        this.min = Number(this.getAttribute('min') ?? 0)
-        this.max = Number(this.getAttribute('max') ?? 100)
-        this.step = Number(this.getAttribute('step') ?? 1)
-        this.input.value = String(this.getAttribute('value') ?? this.min)
-
-        this.minus.addEventListener('click', () => this.changeValue(-1))
-        this.plus.addEventListener('click', () => this.changeValue(1))
         this.input.addEventListener('focusout', () => this.validateValue())
 
-        this.decimalPlaces = this.step.toString().split('.')[1]?.length || 0
 
-        // const sliderId = this.getAttribute('slider-id')
-        // if (sliderId) this.slider = document.getElementById(sliderId) as HTMLInputElement
+        
+        this.minus = this.shadowRoot!.querySelector('.minus')!
+        this.plus = this.shadowRoot!.querySelector('.plus')!
 
-        const btnColor = this.getAttribute('button-color')
-        if (btnColor) {
-            this.minus.style.backgroundColor = btnColor
-            this.plus.style.backgroundColor = btnColor
-        }
+        this.minus.addEventListener('click', this.decrement.bind(this))
+        this.plus.addEventListener('click', this.increment.bind(this))
     }
 
-    get min() { return Number(this.getAttribute('min')) }
-    set min(val: number) { this.setAttribute('min', val.toString()) }
-
-    get max() { return Number(this.getAttribute('max')) }
-    set max(val: number) { this.setAttribute('max', val.toString()) }
-
-    get step() { return Number(this.getAttribute('step')) }
-    set step(val: number) { this.setAttribute('step', val.toString()) }
-
-    changeValue(multiplier: number) {
-        let value = Number(this.input.value) + this.step * multiplier
-        value = Math.min(this.max, Math.max(this.min, value))
-        this.input.value = value.toFixed(this.decimalPlaces)
-        // if (this.slider) this.slider.value = this.input.value
+    decrement() {
+        let newValue = Number(this.input.value) - this.nStep
+        newValue = Math.max(Number(this.nMin), parseFloat(newValue.toFixed(this.decimalPlaces)))
+        this.updateValue(newValue)
     }
 
-    validateValue() {
-        let value = Number(this.input.value)
-        value = Math.min(this.max, Math.max(this.min, Math.round(value / this.step) * this.step))
-        this.input.value = value.toFixed(this.decimalPlaces)
-        // if (this.slider) this.slider.value = this.input.value
+    increment() {
+        let newValue = Number(this.input.value) + this.nStep
+        newValue = Math.min(Number(this.nMax), parseFloat(newValue.toFixed(this.decimalPlaces)))
+        this.updateValue(newValue)
+    }
+
+    updateValue(value: number) {
+        const formattedValue = parseFloat(value.toFixed(this.decimalPlaces))
+        this.input.value = formattedValue.toString()
     }
 }
 
