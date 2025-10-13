cf4ef683a53f2312b6be7dbc1dc85d51cadf9427
unknown <nami0879@naver.com>
2025-10-13 18:47:24 +0900

    Support dynamic attributes via getters and setters

--- examples/main.ts
+++ examples/main.ts
@@ -1,7 +1,20 @@
 import * as aero from '../src'
-// const dynamicContainer = document.getElementById('app-dynamic')
+const dynamicContainer = document.getElementById('dynamic-container')
 
 
 
-// const numericInput = new aero.NumericInput()
-// dynamicContainer?.appendChild(numericInput)
+const numericInput = new aero.NumericInput()
+dynamicContainer?.appendChild(numericInput)
+
+numericInput.style.height = '10px'
+numericInput.step = 2
+setTimeout(() => {
+    numericInput.step = 0.2
+}, 10000)
+
+const resizeBox = new aero.ResizeBox()
+dynamicContainer?.appendChild(resizeBox)
+
+setTimeout(() => {
+    resizeBox.minWidth = 40
+}, 3000)
--- src/components/ui/resize_box/ResizeBox.ts
+++ src/components/ui/resize_box/ResizeBox.ts
@@ -2,8 +2,8 @@ import BaseResizeBox from "../../base/BaseResizeBox"
 import resizeBoxHtmlTemplate from './resize_box.html?raw'
 
 export default class ResizeBox extends BaseResizeBox {
-    constructor() {
-        super(resizeBoxHtmlTemplate)
+    constructor(resizeDirections?: { top?: boolean, bottom?: boolean, left?: boolean, right?: boolean }) {
+        super(resizeBoxHtmlTemplate, resizeDirections)
     }
 }
 
