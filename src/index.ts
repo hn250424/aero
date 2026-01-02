ebe6dc642c9058d1a5a469a6b60fd7a681afef16
unknown <nami0879@naver.com>
2026-01-03 01:49:23 +0900

    feat: add global type definitions for custom elements

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,22 @@ export { AeroSpinbox } from "./components/spinbox/AeroSpinbox";
 export { AeroProgressSpinner } from "./components/progress_spinner/AeroProgressSpinner";
 export { AeroResizeBox } from "./components/resize_box/AeroResizeBox";
 export { AeroSelect } from "./components/select/AeroSelect";
+export { AeroOption } from "./components/select/AeroOption";
+
+import type { AeroNumericInput } from "./components/numeric_input/AeroNumericInput";
+import type { AeroSpinbox } from "./components/spinbox/AeroSpinbox";
+import type { AeroProgressSpinner } from "./components/progress_spinner/AeroProgressSpinner";
+import type { AeroResizeBox } from "./components/resize_box/AeroResizeBox";
+import type { AeroSelect } from "./components/select/AeroSelect";
+import type { AeroOption } from "./components/select/AeroOption";
+
+declare global {
+	interface HTMLElementTagNameMap {
+		"aero-numeric-input": AeroNumericInput;
+		"aero-spinbox": AeroSpinbox;
+		"aero-progress-spinner": AeroProgressSpinner;
+		"aero-resize-box": AeroResizeBox;
+		"aero-select": AeroSelect;
+		"aero-option": AeroOption;
+	}
+}
