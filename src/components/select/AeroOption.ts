43873245f8670d13ea587f2f69df0807f983549a
unknown <nami0879@naver.com>
2026-01-08 19:10:06 +0900

    feat: add JSDoc for AeroSelect and AeroOption components

--- src/components/select/AeroOption.ts
+++ src/components/select/AeroOption.ts
@@ -1,8 +1,26 @@
+/**
+ * @module components
+ */
+
+/**
+ * `<aero-option>` is a custom element used within `<aero-select>` to represent a selectable option.
+ *
+ * @element aero-option
+ *
+ * @slot - Default slot for the option's visible text content.
+ *
+ * @extends HTMLElement
+ */
 export class AeroOption extends HTMLElement {
 	constructor() {
 		super()
 	}
 
+	/**
+	 * The value associated with this option, similar to the `value` attribute on a standard `<option>`.
+	 * @type {string}
+	 * @attr value
+	 */
 	get value(): string {
 		return this.getAttribute("value") ?? "";
 	}
@@ -11,6 +29,10 @@ export class AeroOption extends HTMLElement {
 		this.setAttribute("value", v);
 	}
 
+	/**
+	 * The visible text label of the option. This is typically the text content of the element.
+	 * @type {string}
+	 */
 	get label(): string {
 		return this.textContent ?? "";
 	}
