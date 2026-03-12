8e9e9d5946168c1edb519cbbece7d98a948a0471
unknown <nami0879@naver.com>
2026-03-12 18:30:12 +0900

    feat(site): migrate to path-based routing using History API

--- site/categories/home/setupHomeNavigation.ts
+++ site/categories/home/setupHomeNavigation.ts
@@ -1,7 +1,8 @@
 import { AppContext } from "@site/AppContext";
+import { navigateTo } from "@site/navigation";
 
 export function setupHomeNavigation(appContext: AppContext) {
 	appContext.$logoImg.addEventListener("click", () => {
-		appContext.switchCategory("home");
+		navigateTo(appContext, "/");
 	});
 }
--- site/index.ts
+++ site/index.ts
@@ -1,5 +1,14 @@
 import "./styles/index.css";
 import { init } from "./app";
 
+// Redirect restoration
+(function restorePath() {
+	const params = new URLSearchParams(window.location.search);
+	const path = params.get("path");
+	if (path) {
+		window.history.replaceState(null, "", path);
+	}
+})();
+
 document.addEventListener("DOMContentLoaded", init);
 
